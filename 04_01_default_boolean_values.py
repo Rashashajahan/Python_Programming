7563b6853c1f97f8bd8301edd02bd0da066340ad
Rasha Shajahan <75829999+Rashashajahan@users.noreply.github.com>
2022-12-18 17:26:00 +0530

    Update 04_01_default_boolean_values.py

--- 04_01_default_boolean_values.py
+++ 04_01_default_boolean_values.py
@@ -3,11 +3,21 @@
 
 # make
 # an int
+a=5
+print(bool(a))
 # a float
+b=1.5
+print(bool(b))
 # a string
+c="hiii"
+print(bool(c))
 # the int 0
+d=0
+print(bool(d))
 # the int 1
+e=1
+print(bool(e))
 # the int 1000
-
+f=1000
+print(bool(f))
 # now print out all the `bool()` values using the bool() function
-# are you surprised at the default boolean value for any python type?
