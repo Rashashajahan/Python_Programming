166bdf4e8f842e649cc7052bc0428cc27d560ed1
Rasha Shajahan <75829999+Rashashajahan@users.noreply.github.com>
2022-12-18 17:30:57 +0530

    Update 07_04_string_methods.py

--- 07_04_string_methods.py
+++ 07_04_string_methods.py
@@ -6,3 +6,12 @@ file_1 = "operators.pdf"
 file_2 = "snowfall.jpg"
 file_3 = "uncle-joes-wedding.doc"
 file_4 = "invitation.pdf"
+def check(a):
+    if(a[-4:len(a)]==".pdf"):
+        return(f"{a} is .pdf file.")
+    else:
+        return(f"{a} is not a .pdf file.")
+print(check("operators.pdf"))
+print(check("snowfall.jpg"))
+print(check("uncle-joes-wedding.doc"))
+print(check("invitation.pdf"))
