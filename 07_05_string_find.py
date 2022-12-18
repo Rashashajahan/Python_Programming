af8da8e7e091f1a754251996200c54d8ee7310dd
Rasha Shajahan <75829999+Rashashajahan@users.noreply.github.com>
2022-12-18 17:31:23 +0530

    Update 07_05_string_find.py

--- 07_05_string_find.py
+++ 07_05_string_find.py
@@ -1,3 +1,7 @@
 # Find the index of the first occurrence of the letter `n` in the string.
 
 composer = "Ludvig van Beethoven"
+for i in range(0, len(composer)):
+    if(composer[i]=="n"):
+        print(i)
+        break
