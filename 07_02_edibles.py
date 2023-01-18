ad62af4b4488ab90a0eaa85e7f096bd34c7024ec
Rasha Shajahan <75829999+Rashashajahan@users.noreply.github.com>
2023-01-18 11:43:45 +0530

    added two more edible food

--- 07_02_edibles.py
+++ 07_02_edibles.py
@@ -10,4 +10,6 @@ food1=s[5:9]+s[11]
 food2=s[7:12]
 food3=s[-10:-4]
 food4=s[-21:-14]
-print(food1, food2, food3, food4)
+food5=s[-9]+s[-4:-1]
+food6=s[26:29]
+print(food1, food2, food3, food4, food5, food6)
