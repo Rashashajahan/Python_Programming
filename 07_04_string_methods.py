090bc90fe0cb75be1469f33622b50f0a262fe6a5
Rasha Shajahan <75829999+Rashashajahan@users.noreply.github.com>
2023-01-18 12:01:36 +0530

    added endswith()

--- 07_04_string_methods.py
+++ 07_04_string_methods.py
@@ -15,3 +15,10 @@ print(check("operators.pdf"))
 print(check("snowfall.jpg"))
 print(check("uncle-joes-wedding.doc"))
 print(check("invitation.pdf"))
+
+
+############OR############
+if(file_1.endswith(".pdf")):
+    print(f"{file_1} is a pdf file")
+else:
+    print(f"{file_1} is not a pdf file")
